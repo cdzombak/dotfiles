b94d7b4a6ce3386196cb1c9d0c0020170f5ff553
Chris Dzombak <chris@chrisdzombak.net>
2018-01-21 11:14:37 -0500

    Add bundle ID function for zsh

--- zsh/.zsh/osx.zsh
+++ zsh/.zsh/osx.zsh
@@ -69,6 +69,27 @@ function trash() {
   IFS=$temp_ifs
 }
 
+# Find bundle ID for an app, via http://brettterpstra.com/2012/07/31/overthinking-it-fast-bundle-id-retrieval-for-mac-apps/
+bid() {
+  local shortname location
+
+  # combine all args as regex
+  # (and remove ".app" from the end if it exists due to autocomplete)
+  shortname=$(echo "${@%%.app}"|sed 's/ /.*/g')
+  # if the file is a full match in apps folder, roll with it
+  if [ -d "/Applications/$shortname.app" ]; then
+    location="/Applications/$shortname.app"
+  else # otherwise, start searching
+    location=$(mdfind -onlyin /Applications -onlyin ~/Applications -onlyin /Developer/Applications 'kMDItemKind==Application'|awk -F '/' -v re="$shortname" 'tolower($NF) ~ re {print $0}'|head -n1)
+  fi
+  # No results? Die.
+  [[ -z $location || $location = "" ]] && echo "$1 not found, I quit" && return
+  # Otherwise, find the bundleid using spotlight metadata
+  bundleid=$(mdls -name kMDItemCFBundleIdentifier -r "$location")
+  # return the result or an error message
+  [[ -z $bundleid || $bundleid = "" ]] && echo "Error getting bundle ID for \"$@\"" || echo "$location: $bundleid"
+}
+
 # Show/hide hidden files in the Finder
 alias finder-show-invisibles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias finder-hide-invisibles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
