d28581621386e8d46b898ed7287a69f81d0ed363
Chris Dzombak <chris@chrisdzombak.net>
2018-02-19 16:19:27 -0500

    add wwwmetar function

--- zsh/.zsh/wx.zsh
+++ zsh/.zsh/wx.zsh
@@ -9,3 +9,13 @@ function wx() {
 
 # metar from https://github.com/RyuKojiro/metar
 alias metar='metar -d'
+
+function wwwmetar() {
+	if [[ $# -eq 0 ]]; then
+		curl "http://tgftp.nws.noaa.gov/data/observations/metar/stations/KARB.TXT"
+	else
+		local input="$1"
+		local station=$( awk '{print toupper($0)}' <<< $input )
+		curl "http://tgftp.nws.noaa.gov/data/observations/metar/stations/$station.TXT"
+	fi
+}
