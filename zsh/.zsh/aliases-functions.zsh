62c372354243e1baa0f2d2de8fbaee29aa6209d5
Chris Dzombak <chris@chrisdzombak.net>
2019-03-25 10:44:35 -0400

    add one off feedbin archiver command

--- zsh/.zsh/aliases-functions.zsh
+++ zsh/.zsh/aliases-functions.zsh
@@ -33,6 +33,9 @@ alias git-stage-missing="git status | grep deleted | awk '{print \$3}' | xargs g
 # wake PC via SSH into curie-srv at home
 alias wol-edison="ssh -t curie-remote \"/usr/local/bin/wakeonlan -i 192.168.1.255 -p 7 70:85:c2:22:b5:0b\""
 
+# one-off run feedbin archiver (https://github.com/cdzombak/feedbin-auto-archiver)
+alias archive-feedbin="ssh -t burr \"/home/cdzombak/scripts/feedbin-auto-archiver/venv/bin/python3 /home/cdzombak/scripts/feedbin-auto-archiver/feedbin_archiver.py --rules-file /home/cdzombak/Dropbox/feedbin-archiver-rules.json --dry-run false\""
+
 # find external IP. pass -4 or -6 to specify v4/v6 address.
 alias myip='curl -s -w "\n" https://ip.dzdz.cz'
 
