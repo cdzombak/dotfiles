f22a9e40aac0c6d712f460ea2319659bb390c365
Chris Dzombak <chris@chrisdzombak.net>
2019-06-05 10:41:22 -0400

    whitespace

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -1,9 +1,9 @@
 # Show history
 case $HIST_STAMPS in
-  "mm/dd/yyyy") alias history='fc -fl 1' ;;
-  "dd.mm.yyyy") alias history='fc -El 1' ;;
-  "yyyy-mm-dd") alias history='fc -il 1' ;;
-  *) alias history='fc -l 1' ;;
+    "mm/dd/yyyy") alias history='fc -fl 1' ;;
+    "dd.mm.yyyy") alias history='fc -El 1' ;;
+    "yyyy-mm-dd") alias history='fc -il 1' ;;
+    *) alias history='fc -l 1' ;;
 esac
 
 # grep history
