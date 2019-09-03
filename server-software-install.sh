155a91aee60a970a0e3a844169d0e1225bdd642b
Chris Dzombak <chris@chrisdzombak.net>
2019-09-03 16:24:45 -0400

    fix small errors in Linux software install script

--- server-software-install.sh
+++ server-software-install.sh
@@ -6,13 +6,15 @@ if [ "$(uname)" == "Darwin" ]; then
   exit 2
 fi
 
+mkdir -p "$HOME/opt/bin"
+
 # install dust: A more intuitive version of du in rust
-if [ ! -x "/usr/local/bin/dust" ]; then
-  TMP_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'dust-work'`
+if [ ! -x "$HOME/opt/bin/dust" ]; then
+  TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'dust-work')
   pushd "$TMP_DIR"
   curl -s https://api.github.com/repos/bootandy/dust/releases/latest | jq -r ".assets[].browser_download_url" | grep "linux" | xargs wget -q -O dust.tar.gz
   tar xzf dust.tar.gz
-  cp dust $HOME/opt/bin
+  cp dust "$HOME/opt/bin"
   popd
 fi
 
