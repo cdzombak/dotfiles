ab275a2c9eeeebccdb226fedae2fe4abe2af1c92
Chris Dzombak <chris@dzombak.com>
2020-01-14 14:35:19 -0500

    tidal

--- macos-software-install.sh
+++ macos-software-install.sh
@@ -267,6 +267,7 @@ sw_install /Applications/Slack.app "brew_cask_install slack" \
 sw_install /Applications/Sonos.app "brew_cask_install sonos"
 sw_install /Applications/Spotify.app "brew_cask_install spotify"
 sw_install "/Applications/Sublime Merge.app" "brew_cask_install sublime-merge"
+sw_install /Applications/TIDAL.app "brew_cask_install tidal"
 sw_install "/Applications/The Unarchiver.app" "brew_cask_install the-unarchiver"
 sw_install "/Applications/Transmit.app" "brew_cask_install transmit" \
   "- [ ] License\n- [ ] Sign into Panic Sync"
