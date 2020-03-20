d50d3c1d4415035d557d59f846abbfba106be0d4
Chris Dzombak <chris@dzombak.com>
2020-03-20 12:55:50 -0400

    fzf is already in path

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -8,12 +8,6 @@ export FZF_DEFAULT_COMMAND='ag -g ""'
 ### [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 ###
 
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
