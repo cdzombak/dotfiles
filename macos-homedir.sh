1e64171b4b5a2fcad992b5d12f6dd5f7ec825438
Chris Dzombak <chris@dzombak.com>
2020-03-27 10:05:22 -0400

    icloud link in ~/pictures

--- macos-homedir.sh
+++ macos-homedir.sh
@@ -68,6 +68,10 @@ if [ ! -L "$HOME/Downloads/iCloud" ]; then
   ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Downloads" "$HOME/Downloads/iCloud"
 fi
 
+if [ ! -L "$HOME/Pictures/iCloud" ]; then
+  ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Pictures" "$HOME/Pictures/iCloud"
+fi
+
 if [ ! -L "$HOME/Books and Articles" ] && [ ! -e "$HOME/.local/dotfiles/no-home-booksandarticles-dir" ] ; then
   echo ""
   echo "Create link to iCloud Drive/Books & Articles in home directory? (y/N)"
