675a33c185785852fef0dd21be161d17d1e29462
Chris Dzombak <chris@dzombak.com>
2021-04-12 16:05:56 -0400

    optional install: Port Map app (UPnP debugging)

--- macos-software-install.sh
+++ macos-software-install.sh
@@ -552,9 +552,22 @@ _install_unifiprotect() {
 }
 sw_install "/Applications/UniFi Protect.app" _install_unifiprotect
 
+_install_portmap() {
+  local LATEST_PORTMAP_TAG="PortMap-2.0.1"
+  set -x
+  TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'portmap-work')
+  pushd "$TMP_DIR"
+  curl -s "https://api.github.com/repos/monkeydom/TCMPortMapper/releases/tags/$LATEST_PORTMAP_TAG" | jq -r ".assets[].browser_download_url" | grep "PortMap" | grep ".zip" | xargs wget -q -O portmap.zip
+  unzip portmap.zip
+  rm portmap.zip
+  cp -R "Port Map.app" "/Applications/Port Map.app"
+  popd
+  set +x
+}
+
 echo ""
 cecho "Install network tools? (y/N)" $magenta
-echo "(Angry IP Scanner, Discovery, dog [CLI DNS client], iperf3, mtr, nmap, speedtest, telnet, Wifi Explorer)"
+echo "(Angry IP Scanner, Discovery, dog [CLI DNS client], iperf3, mtr, nmap, Port Map, speedtest, telnet, Wifi Explorer)"
 read -r response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
   sw_install "/Applications/Angry IP Scanner.app" "brew_cask_install angry-ip-scanner"
@@ -566,6 +579,8 @@ if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
   sw_install "$(brew --prefix)/bin/speedtest" "brew_install speedtest"
   sw_install "$(brew --prefix)/bin/telnet" "brew_install telnet"
   sw_install "/Applications/WiFi Explorer.app" "mas install 494803304"
+
+  sw_install "/Applications/Port Map.app" _install_portmap
 
   echo ""
   cecho "Install Wireshark & Bettercap? (y/N)" $magenta
