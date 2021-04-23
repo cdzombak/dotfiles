18da38ab90c3bfdc55718739df3706beeb5b242f
Chris Dzombak <chris@dzombak.com>
2021-04-23 09:55:45 -0400

    don't show safari internal debug menu

--- macos-configure.sh
+++ macos-configure.sh
@@ -360,7 +360,7 @@ killall Dock
 
 echo ""
 echo "Enable Safari's debug & development features"
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool false
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
