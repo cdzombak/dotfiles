1c0cd981dd995e81cf6d9c1186e9bb46a606e34c
Chris Dzombak <chris@dzombak.com>
2021-11-04 12:14:37 -0400

    add Paint S app

--- macos-software-install.sh
+++ macos-software-install.sh
@@ -388,6 +388,8 @@ sw_install /Applications/NewFileMenu.app "mas install 1064959555" \
   "- [ ] Enable Finder extension\n- [ ] Enable opening file after creation\n- [ ] Disable menu bar item\n- [ ] Disable all templates except plain text and shell script\n- [ ] Add Markdown template (located in Sync/Configs/NewFileMenu)"
 sw_install /Applications/Numbers.app "mas install 409203825"
 sw_install /Applications/Pages.app "mas install 409201541"
+sw_install "/Applications/Paint S.app" "mas install 736473980" \
+  "- [ ] Restore purchases"
 sw_install /Applications/Pastebot.app "mas install 1179623856" \
   "- [ ] Start at login\n- [ ] Set/confirm Shift-Command-V global shortcut\n- [ ] Configure, especially Always Paste Plain Text\n- [ ] Enable Accessibility control"
 sw_install /Applications/Peek.app "mas install 1554235898" \
