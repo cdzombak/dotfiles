408eaabf65c67966c355d7d97b7d49071cb911f0
Chris Dzombak <chris@dzombak.com>
2022-07-25 15:59:54 -0400

    QA pass on monitor switching hammerspoon module

--- hammerspoon/.hammerspoon/desk-usb.lua
+++ hammerspoon/.hammerspoon/desk-usb.lua
@@ -1,4 +1,4 @@
-local log = hs.logger.new('desk-monitor.lua', 'debug')
+local log = hs.logger.new('desk-usb.lua', 'debug')
 
 function usbCallback(data)
     if data["eventType"] == "added" then
@@ -15,15 +15,17 @@ function usbCallback(data)
             elseif string.find(output, "curie", 0, true) then
                 isHomeDeskMacStudio = true
             end
+            log.d("isHomeDeskMacStudio: " .. tostring(isHomeDeskMacStudio))
 
             -- Is this machine currently connected to my home office desk external monitor?
             local isHomeDeskExternalMonitor = false
             local output, status = hs.execute("/usr/local/bin/lunar get serial")
             if status == false then
                 log.d("failed to get monitor serial: " .. output)
-            elseif string.find(output, "B5545C3D-AA52-422C-8C50-2D97E231D7F3", 0, true) then
+            elseif string.find(output, "B5545C3D-AA52-422C-8C50-2D97E231D7F3", 0, true) or string.find(output, "3C82E6B9-5051-42FD-8BA8-3BB83EC50EE8", 0, true) then
                 isHomeDeskExternalMonitor = true
             end
+            log.d("isHomeDeskExternalMonitor: " .. tostring(isHomeDeskExternalMonitor))
 
             -- on disconnect from 'Freestyle Edge Keyboard',
             -- if this machine is my personal secondary desktop Mac
