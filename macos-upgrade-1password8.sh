c75dd26aca59e643eb23f75d0e821f1b33caa3b8
Chris Dzombak <chris@dzombak.com>
2022-12-16 10:23:54 -0500

    update 1p8 upgrade steps

--- macos-upgrade-1password8.sh
+++ macos-upgrade-1password8.sh
@@ -6,22 +6,25 @@ if [ "$(uname)" != "Darwin" ]; then
   exit 2
 fi
 
+killall "1Password 7"
+
 brew reinstall --cask 1password  # /Applications/1Password.app
 mas install 1569813296  # "/Applications/1Password for Safari.app"
 brew install --cask 1password-cli  # /usr/local/bin/op
 
 echo "# 1Password 8 Post-Upgrade Setup" > "$HOME/1Password8.md"
 echo "- [ ] Sign in to accounts: personal; work as needed" >> "$HOME/1Password8.md"
+echo "- [ ] Arrange in Dock" >> "$HOME/1Password8.md"
 echo "- [ ] Do not show in menu bar" >> "$HOME/1Password8.md"
 echo "- [ ] Start at login" >> "$HOME/1Password8.md"
 echo "- [ ] No keyboard shortcut for: Show 1Password; Lock 1Password" >> "$HOME/1Password8.md"
 echo "- [ ] Quick Access: Ctrl-Shift-Command-Backslash" >> "$HOME/1Password8.md"
 echo "- [ ] Autofill: Command-Backslash" >> "$HOME/1Password8.md"
 echo "- [ ] Appearance -> Density: Compact" >> "$HOME/1Password8.md"
-echo "- [ ] Security: enable unlock with Apple Watch" >> "$HOME/1Password8.md"
-echo "- [ ] Security: hold Option to toggle revealed fields" >> "$HOME/1Password8.md"
+echo "- [ ] Security: Enable unlock with Touch ID and Apple Watch" >> "$HOME/1Password8.md"
+echo "- [ ] Security: Hold Option to toggle revealed fields" >> "$HOME/1Password8.md"
 echo "- [ ] Privacy -> Watchtower: Enable all Watchtower features" >> "$HOME/1Password8.md"
-echo "- [ ] Developer: enable biometric unlock for CLI" >> "$HOME/1Password8.md"
+echo "- [ ] Developer: connect with CLI" >> "$HOME/1Password8.md"
 echo "" >> "$HOME/1Password8.md"
 echo "## 1Password for Safari" >> "$HOME/1Password8.md"
 echo "- [ ] Enable in Safari" >> "$HOME/1Password8.md"
