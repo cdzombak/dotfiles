7063dfc43dcc2b222d009e2620a15ffd97543e88
Chris Dzombak <chris@dzombak.com>
2023-04-01 21:24:46 -0400

    add tamron tap-in utility to dock photos folder

--- macos-home-applications.sh
+++ macos-home-applications.sh
@@ -198,6 +198,9 @@ fi
 if [ -e "/Applications/SIGMA Photo Pro 6.app" ] && [ ! -e "$HOME/Applications/Photo Tools/Sigma Photo Pro" ]; then
   ln -s "/Applications/SIGMA Photo Pro 6.app" "$HOME/Applications/Photo Tools/Sigma Photo Pro"
 fi
+if [ -e "/Applications/TAP-in Utility.app" ] && [ ! -e "$HOME/Applications/Photo Tools/Tamron Tap-In Utility" ]; then
+  ln -s "/Applications/TAP-in Utility.app" "$HOME/Applications/Photo Tools/Tamron Tap-In Utility"
+fi
 if [ -e "/Applications/Neat Image v9 Standalone/Neat Image v9.app" ] && [ ! -e "$HOME/Applications/Photo Tools/Neat Image" ]; then
   ln -s "/Applications/Neat Image v9 Standalone/Neat Image v9.app" "$HOME/Applications/Photo Tools/Neat Image"
 fi
