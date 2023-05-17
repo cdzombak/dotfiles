4c4e3035cc109df5073c48af6a5479e2cd987e73
Chris Dzombak <chris@dzombak.com>
2023-05-17 12:38:09 -0400

    add aws_profile to rprompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -251,7 +251,7 @@ _zsh_rprompt_aws_profile() {
   if [[ -n "$AWS_PROFILE" ]]; then
     if [[ "$AWS_PROFILE" =~ "^prod" ]]; then
         _zsh_rprompt_segment red black "${AWS_PROFILE#*_}"
-    if [[ "$AWS_PROFILE" =~ "^internal_panther-dev-" ]]; then
+    elif [[ "$AWS_PROFILE" =~ "^internal_panther-dev-" ]]; then
         _zsh_rprompt_segment green black "${AWS_PROFILE#*internal_panther-dev-}"
     else
         _zsh_rprompt_segment green black "${AWS_PROFILE#*_}"
