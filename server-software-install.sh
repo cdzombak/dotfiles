1c42a06724e73e51f4d77e18bf0c8757846803c2
Chris Dzombak <chris@dzombak.com>
2023-09-28 17:00:00 -0400

    revamp server software install script

--- server-software-install.sh
+++ server-software-install.sh
@@ -1,232 +1,177 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# versions:
-LATEST_BANDWHICH="0.20.0"  # https://github.com/imsnif/bandwhich/releases
-LATEST_RESTIC="0.16.0"  # https://github.com/restic/restic/releases
-NANO_V="7.0"  # https://www.nano-editor.org/download.php
-NANO_MAJOR_V="7"
-
 if [ "$(uname)" != "Linux" ]; then
   echo "Skipping Linux software setup because not on Linux"
   exit 2
 fi
 
+if [ ! -x /usr/bin/apt ]; then
+  echo "Skipping Linux software setup because /usr/bin/apt is not available"
+  exit 1
+fi
+
+# Authenticate upfront and run a keep-alive to update `sudo` time stamp until script has finished
 echo "This script will use sudo; enter your password to authenticate if prompted."
-# Authenticate upfront and run a keep-alive to update existing `sudo` time stamp until script has finished
 sudo -v
 while true; do sudo -v -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-mkdir -p "$HOME/opt/bin"
-
-if [ -x /usr/bin/apt ]; then
-  echo "Installing common packages via apt..."
-  set -x
-  sudo apt -y update
-  sudo apt -y install tig tree htop traceroute dnsutils screen molly-guard
-  set +e
-  echo "nnn is not available until Ubuntu 18.x; this installation is expected to fail on earlier systems:"
-  sudo apt -y install nnn
-  set -e
-  set +x
-elif [ -x /usr/bin/dnf ]; then
-  echo "Installing common packages via dnf..."
-  set -x
-  sudo dnf -y install tig tree htop nnn traceroute bind-utils screen molly-guard
-  set +x
-elif [ -x /usr/bin/yum ]; then
-  echo "Installing common packages via yum..."
-  set -x
-  sudo yum -y install tig tree htop nnn traceroute bind-utils screen molly-guard
-  set +x
-else
-  echo ""
-  echo "[!] Could not find apt, dnf, or yum; don't know how to install packages on this system."
-  echo ""
+if [ ! -f /etc/apt/sources.list.d/cdzombak_oss_any.list ]; then
+  echo "Setting up cdzombak/oss/any PackageCloud apt repo..."
+  curl -s https://packagecloud.io/install/repositories/cdzombak/oss/script.deb.sh?any=true | sudo bash
+fi
+if [ ! -f /etc/apt/sources.list.d/cdzombak_3p_any.list ]; then
+  echo "Setting up cdzombak/3p/any PackageCloud apt repo..."
+  curl -s https://packagecloud.io/install/repositories/cdzombak/3p/script.deb.sh?any=true | sudo bash
 fi
 
-# install bandwhich: Terminal bandwidth utilization tool
-# see https://github.com/imsnif/bandwhich/releases
-echo "Installing bandwhich..."
-if [ -x "$HOME/opt/bin/bandwhich" ]; then
-  echo " Moving bandwhich from ~ to /usr/local..."
-  sudo mv "$HOME/opt/bin/bandwhich" /usr/local/bin/bandwhich
-fi
-if [ -x /usr/local/bin/bandwhich ]; then
-  # remove outdated version:
-  if ! /usr/local/bin/bandwhich -V | grep -c "$LATEST_BANDWHICH" >/dev/null ; then
-    echo " Removing outdated bandwhich..."
-    sudo rm /usr/local/bin/bandwhich
-  fi
-fi
-if [ ! -x /usr/local/bin/bandwhich ]; then
-  if uname -a | grep -c -i x86_64 >/dev/null; then
-    set -x
-    TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'bandwhich-work')
-    pushd "$TMP_DIR"
-    curl -s "https://api.github.com/repos/imsnif/bandwhich/releases/tags/$LATEST_BANDWHICH" | jq -r ".assets[].browser_download_url" | grep "linux-musl" | grep "x86_64" | xargs wget -q -O bandwhich.tar.gz
-    tar xzf bandwhich.tar.gz
-    rm bandwhich.tar.gz
-    sudo cp ./bandwhich /usr/local/bin
-    sudo chmod +x /usr/local/bin/bandwhich
-    sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep /usr/local/bin/bandwhich
-    popd
-    set +x
-  else
-    echo "[!] Unsupported arch. bandwhich only has a build for x86_64; check https://github.com/imsnif/bandwhich/releases to see if this has changed."
-  fi
-else
-  echo "bandwhich is already installed."
-fi
+sudo apt -y update
 
-# install restic: Fast, secure, efficient backup program
-# see https://github.com/restic/restic/releases
-echo "Installing restic..."
-if [ -x /usr/local/bin/restic ]; then
-  # remove outdated version:
-  if ! /usr/local/bin/restic version | grep -c "$LATEST_RESTIC" >/dev/null ; then
-    echo " Removing outdated restic..."
-    sudo rm /usr/local/bin/restic
-  fi
-fi
-if [ ! -x /usr/local/bin/restic ]; then
-  set -x
-  TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'restic-work')
-  pushd "$TMP_DIR"
-  if uname -a | grep -c -i arm >/dev/null; then
-    # NOTE(cdzombak): arm64 is also available, but I don't have any Linux/arm64 systems ATM.
-    #                 When I do this install process will need to be updated.
-    curl -s "https://api.github.com/repos/restic/restic/releases/tags/v$LATEST_RESTIC" | jq -r ".assets[].browser_download_url" | grep "linux" | grep "arm\." | xargs wget -q -O restic.bz2
-  elif uname -a | grep -c -i x86_64 >/dev/null; then
-    curl -s "https://api.github.com/repos/restic/restic/releases/tags/v$LATEST_RESTIC" | jq -r ".assets[].browser_download_url" | grep "linux" | grep "amd64" | xargs wget -q -O restic.bz2
-  else
-    curl -s "https://api.github.com/repos/restic/restic/releases/tags/v$LATEST_RESTIC" | jq -r ".assets[].browser_download_url" | grep "linux" | grep "386" | xargs wget -q -O restic.bz2
-  fi
-  bunzip2 restic.bz2
-  sudo cp ./restic /usr/local/bin/restic
-  sudo chmod +x /usr/local/bin/restic
-  popd
-  set +x
-else
-  echo "restic is already installed."
-fi
-
-# install my listening wrapper for netstat
-echo "Installing listening..."
-if [ ! -x "/usr/local/bin/listening" ]; then
-  if [ -e "$HOME/opt/bin/listening" ]; then
-    echo " Moving listening from ~ to /usr/local..."
-    sudo mv "$HOME/opt/bin/listening" /usr/local/bin
-  else
-    set -x
-    TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'listening-work')
-    pushd "$TMP_DIR"
-    wget https://gist.githubusercontent.com/cdzombak/fc0c0acbba9c302571add6dcd6d10deb/raw/c607f9fcc182ecc5d0fcc844bff67c1709847b55/listening
-    chmod +x ./listening
-    sudo mv ./listening /usr/local/bin
-    popd
-    set +x
-  fi
-else
-  echo "listening is already installed."
-fi
-
-# install a more recent version of nano
-echo "Installing a recent nano..."
-if [ -x /usr/local/bin/nano ]; then
-  # remove outdated version:
-  if ! /usr/local/bin/nano -V | grep -c "$NANO_V" >/dev/null ; then
-    sudo mkdir -p /usr/local/opt/nano/bin
-    sudo mv /usr/local/bin/nano /usr/local/opt/nano/bin/nano.bak
-  fi
-fi
-if [ ! -x /usr/local/bin/nano ]; then
-  set -x
-  TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'nano-working')
-  pushd "$TMP_DIR"
-
-  if [ -x /usr/bin/apt-get ]; then
-    sudo apt-get -y install build-essential
-    sudo apt-get -y build-dep nano
-  elif [ -x /usr/bin/dnf ]; then
-    sudo dnf -y group install "Development Tools"
-    sudo dnf -y builddep nano
-  elif [ -x /usr/bin/yum ]; then
-    sudo yum -y groupinstall "Development Tools"
-    sudo yum -y builddep nano
-  else
-    echo "[!] Don't know how to install build tools and dependencies. This may fail."
-    echo "    (package manager is not apt, dnf, or yum)"
-  fi
-
-  wget "https://www.nano-editor.org/dist/v$NANO_MAJOR_V/nano-$NANO_V.tar.gz"
-  tar -xzvf "nano-$NANO_V.tar.gz"
-  cd "./nano-$NANO_V"
-
-  ./configure \
-    --prefix=/usr/local \
-    --enable-libmagic \
-    --enable-utf8 \
-    --enable-nanorc \
-    --enable-color \
-    --enable-multibuffer
-  make
-
-  if [ -x /usr/bin/apt-get ]; then
-    sudo apt-get remove nano
-  elif [ -x /usr/bin/dnf ]; then
-    sudo dnf remove nano
-  elif [ -x /usr/bin/yum ]; then
-    sudo yum remove nano
-  else
-    echo "[!] Don't know how to remove the stock nano installation. Do this manually."
-    echo "    (package manager is not apt, dnf, or yum)"
-  fi
-
-  sudo make install-strip
-  sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/nano 50
-
-  popd # $TMP_DIR
-  set +x
-
-  echo ""
-  echo "If necessary, run 'sudo update-alternatives --config editor' to select the new nano installation."
-  echo ""
-else
-  echo "nano is already installed."
-fi
+echo "Installing common packages via apt..."
+sudo apt -y install tig tree htop nnn traceroute dnsutils screen molly-guard nano jq wget
 
 if [ ! -L "$HOME/.local/.nano-root" ]; then
   mkdir -p "$HOME/.local"
-  ln -s /usr/local "$HOME/.local/.nano-root"
+  ln -s /usr "$HOME/.local/.nano-root"
 fi
 
-echo "Install/update my notify-me script? (requires auth to dropbox.dzombak.com/_auth) (y/N)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  if [ -f "$HOME/opt/bin/notify-me" ]; then
-    rm "$HOME/opt/bin/notify-me"
-  fi
-  set -x
-  curl -f -u cdzombak --output "$HOME/opt/bin/notify-me" https://dropbox.dzombak.com/_auth/notify-me
-  chmod 755 "$HOME/opt/bin/notify-me"
-  set +x
-fi
-
-if [ ! -e "$HOME/.config/dotfiles/no-ffmpeg-scripts" ]; then
-  echo "Install/update my quick media conversion scripts? (y/N)"
+if [ ! -e "$HOME/.config/dotfiles/no-fail2ban" ] && ! dpkg-query -W fail2ban >/dev/null; then
+  echo "Install fail2ban? (y/N)"
   read -r response
   if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    set -x
-    TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mediascripts')
-    git clone "https://github.com/cdzombak/quick-media-scripts.git" "$TMP_DIR"
-    pushd "$TMP_DIR"
-    chmod +x ./install.sh
-    ./install.sh
-    popd
-    set +x
+    sudo apt -y install fail2ban
+  else
+    echo "Won't ask again next time this script is run."
+    touch "$HOME/.config/dotfiles/no-fail2ban"
+  fi
+fi
+
+if [ ! -e "$HOME/.config/dotfiles/no-ufw" ] && ! dpkg-query -W ufw >/dev/null; then
+  echo "Install ufw? (y/N)"
+  read -r response
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    sudo apt -y install ufw
+  else
+    echo "Won't ask again next time this script is run."
+    touch "$HOME/.config/dotfiles/no-ufw"
+  fi
+fi
+
+echo "Installing self-packaged software via apt-get..."
+sudo apt-get -y install apply-crontab dirshard listening runner restic
+
+if [ "$(uname -m)" = "x86_64" ]; then
+  sudo apt-get -y install bandwhich
+else
+  echo "[!] Bandwhich: unsupported architecture. Check https://github.com/imsnif/bandwhich/releases to see if non-x86_64 builds are available."
+fi
+
+if [ ! -d "$HOME/crontab.d" ]; then
+  echo "Setting up ~/crontab.d for use with apply-crontab..."
+  apply-crontab -i
+fi
+
+if ! command -v op >/dev/null; then
+  echo "Installing 1Password CLI..."
+  # from https://developer.1password.com/docs/cli/get-started/:
+  # Add the key for the 1Password apt repository:
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+  # Add the 1Password apt repository:
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | sudo tee /etc/apt/sources.list.d/1password.list
+  # Add the debsig-verify policy:
+  sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+  curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+  sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+    sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+  # Install 1Password CLI:
+  sudo apt -y update && sudo apt -y install 1password-cli
+fi
+
+echo "Install/update notify-me script? (y/N)"
+echo "(requires auth to dropbox.dzombak.com/_auth)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  curl -f -u cdzombak --output "$HOME/opt/bin/notify-me" https://dropbox.dzombak.com/_auth/notify-me
+  chmod 755 "$HOME/opt/bin/notify-me"
+fi
+
+_install_docker() {
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/"$(lsb_release -is | tr '[:upper:]' '[:lower:]')"/gpg | sudo apt-key add -
+  DEBARCH=""
+  if [ "$(uname -m)" = "x86_64" ]; then
+    DEBARCH="amd64"
+  elif [ "$(uname -m)" = "aarch64" ]; then
+    DEBARCH="arm64"
+  else
+    echo "[!] This Docker setup script doesn't (yet) support architecture '$(uname -m)'.";
+    echo "    Skipping Docker install for now."
+    return
+  fi
+  sudo add-apt-repository "deb [arch=$DEBARCH] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable"
+  sudo apt -y update
+  apt-cache policy docker-ce
+  echo "Does the above apt-cache policy indicate Docker will be installed from download.docker.com? (y/N)"
+  read -r response
+  if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Okay. Skipping Docker install for now."
+    return
+  fi
+  sudo apt install docker-ce -y
+  if [ ! -e /etc/docker/daemon.json ]; then
+    echo "{}" | sudo tee /etc/docker/daemon.json > /dev/null
+  fi
+  jq ".[\"log-driver\"]=\"local\"" /etc/docker/daemon.json > /tmp/daemon.json.tmp
+  sudo mv /tmp/daemon.json.tmp /etc/docker/daemon.json
+  sudo systemctl enable docker
+  sudo systemctl start docker
+  sudo usermod -aG docker "$(whoami)"
+  mkdir -p "$HOME/opt/docker/compose"
+  mkdir -p "$HOME/opt/docker/data"
+  if [ ! -e "$HOME/opt/docker/compose/.git" ]; then
+    pushd "$HOME/opt/docker/compose" >/dev/null
+    git init
+    popd >/dev/null
+  fi
+}
+if [ ! -e "$HOME/.config/dotfiles/no-docker" ] && ! command -v docker >/dev/null; then
+  echo "Install Docker? (y/N)"
+  read -r response
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    _install_docker
+  else
+    echo "Won't ask again next time this script is run."
+    touch "$HOME/.config/dotfiles/no-docker"
+  fi
+fi
+
+if [ ! -e "$HOME/.config/dotfiles/no-ffmpeg-scripts" ] && ! dpkg-query -W quick-media-conv >/dev/null; then
+  echo "Install quick media conversion scripts? (y/N)"
+  read -r response
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    sudo apt-get install -y quick-media-conv
   else
     echo "Won't ask again next time this script is run."
     touch "$HOME/.config/dotfiles/no-ffmpeg-scripts"
+  fi
+fi
+
+if [ ! -e "$HOME/.config/dotfiles/no-yt-dlp" ] && ! command -v yt-dlp >/dev/null; then
+  echo "Install yt-dlp? (y/N)"
+  read -r response
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+    sudo chmod a+rx /usr/local/bin/yt-dlp
+    cat << EOF | sudo tee /etc/cron.daily/yt-dlp-update >/dev/null
+#!/bin/sh
+/usr/local/bin/yt-dlp -U
+EOF
+    sudo chown root:root /etc/cron.daily/yt-dlp-update
+    sudo chmod 0755 /etc/cron.daily/yt-dlp-update
+  else
+    echo "Won't ask again next time this script is run."
+    touch "$HOME/.config/dotfiles/no-yt-dlp"
   fi
 fi
