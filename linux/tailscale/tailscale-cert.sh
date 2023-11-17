96498b9f5707edcee5714b3afd45812500012bb7
Chris Dzombak <chris@dzombak.com>
2023-11-17 11:21:57 -0500

    remove unneeded log from tailscale-cert script

--- linux/tailscale/tailscale-cert.sh
+++ linux/tailscale/tailscale-cert.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 TS_IP=$(dig @100.100.100.100 +noall +answer +short -x  "$(tailscale ip -1)" | sed 's/.$//')
 RESULT=$(tailscale cert --cert-file=/opt/tailscale/cert/cert.pem --key-file=/opt/tailscale/cert/key.pem "$TS_IP")
 if [[ "$RESULT" == *"cert unchanged"* ]]; then
-  echo "Cert is unchanged; skipping followup tasks."
   exit 0
 fi
 
