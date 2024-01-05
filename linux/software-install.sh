31c61299697d708470487ce72d726440edec45a9
Chris Dzombak <chris@dzombak.com>
2024-01-04 19:07:25 -0600

    tee >/dev/null

--- linux/software-install.sh
+++ linux/software-install.sh
@@ -126,7 +126,7 @@ if ! command -v op >/dev/null; then
   # Add the debsig-verify policy:
   sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
   curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol >/dev/null
   sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
   curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
     sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
