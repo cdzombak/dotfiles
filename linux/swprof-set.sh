16bc2e22cbdb770bdebe7c82a99d6421877ecb21
Chris Dzombak <chris@dzombak.com>
2024-01-08 13:03:59 -0500

    linux: root does not have its own swprof
    kind of a hack, but this works for my machines' setup

--- linux/swprof-set.sh
+++ linux/swprof-set.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! compgen -G "$HOME/.config/dotfiles/sw-profile*" > /dev/null; then
-  mkdir -p "$HOME/.config/dotfiles"
+IS_ROOT=false
+if [ "$EUID" -eq 0 ]; then
+  if [ "$HOME" != "/root" ]; then
+    echo "[!] you appear to be root but HOME is not /root; exiting."
+    exit 1
+  fi
+  IS_ROOT=true
+fi
+
+if ! compgen -G "/home/cdzombak/.config/dotfiles/sw-profile*" > /dev/null; then
+  mkdir -p "/home/cdzombak/.config/dotfiles"
+  if $IS_ROOT; then chown cdzombak:cdzombak "/home/cdzombak/.config/dotfiles"; fi
 
   PS3="Select a profile for this system: "
   select opt in Desktop "Home Server" "Public Server"; do
     case $opt in
       Desktop)
-        touch "$HOME/.config/dotfiles/sw-profile-desktop"
+        touch "/home/cdzombak/.config/dotfiles/sw-profile-desktop"
         break
         ;;
       "Home Server")
-        touch "$HOME/.config/dotfiles/sw-profile-home-server"
+        touch "/home/cdzombak/.config/dotfiles/sw-profile-home-server"
         break
         ;;
       "Public Server")
-        touch "$HOME/.config/dotfiles/sw-profile-public-server"
+        touch "/home/cdzombak/.config/dotfiles/sw-profile-public-server"
         break
         ;;
       *)
@@ -24,4 +34,5 @@ if ! compgen -G "$HOME/.config/dotfiles/sw-profile*" > /dev/null; then
         ;;
     esac
   done
+  if $IS_ROOT; then chown cdzombak:cdzombak "/home/cdzombak/.config/dotfiles/sw-profile*"; fi
 fi
