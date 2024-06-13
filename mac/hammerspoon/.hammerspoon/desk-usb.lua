9494281e8c855ef41e1d3888a41fab3a6c468df7
Chris Dzombak <chris@dzombak.com>
2024-06-13 15:30:54 -0400

    auto mute studio display speakers when switching to work computer

--- mac/hammerspoon/.hammerspoon/desk-usb.lua
+++ mac/hammerspoon/.hammerspoon/desk-usb.lua
@@ -1,10 +1,5 @@
 local log = hs.logger.new('desk-usb.lua', 'debug')
 
--- function file_exists(name)
---    local f=io.open(name, "r")
---    if f~=nil then io.close(f) return true else return false end
--- end
-
 function usbCallback(data)
   local isMainKeyboard = string.find(data["productName"], "Freestyle Edge Keyboard", 0, true)
   local isMainMouse = string.find(data["productName"], "USB Optical Mouse", 0, true) and string.find(data["vendorID"], "7119", 0, true)
@@ -12,38 +7,7 @@ function usbCallback(data)
   local isEyeContactWebcam = string.find(data["productName"], "iContact Camera Pro", 0, true)
   local isWebcam = isLogitechWebcam or isEyeContactWebcam
 
-  if data["eventType"] == "added" then
-    log.d("USB connect: productName '" .. data["productName"] .. "'; vendorID '" .. data["vendorID"] .. "'; productID '" .. data["productID"] .. "'")
-
-    -- if isMainMouse then
-    --   -- wake this machine
-    --   log.d("waking machine via /usr/bin/caffeinate...")
-    --   hs.task.new('/usr/bin/caffeinate', nil, {"-u", "-t", "10"}):start()
-    -- end
-
-    -- if isWebcam then
-      -- start webcam/videoconf support software:
-      -- hs.application.open("com.corsair.ControlCenter")
-
-      -- handMirrorApp = hs.application.get("net.rafaelconde.Hand-Mirror")
-      -- if handMirrorApp == nil then
-      --   hs.application.open("net.rafaelconde.Hand-Mirror")
-      -- end
-      -- if isEyeContactWebcam then
-      --   hs.application.open("com.mbox.iContactControl")
-      -- end
-      -- if isLogitechWebcam then
-      --   logiTuneApp = hs.application.open("com.logitech.logitune", 4, true)
-      --   if logiTuneApp then
-      --     for _, window in pairs(logiTuneApp:visibleWindows()) do
-      --       window:close()
-      --     end
-      --   else
-      --     log.d("LogiTune not up after launch wait timeout; cannot close window automatically")
-      --   end
-      -- end
-    -- end
-  elseif data["eventType"] == "removed" then
+  if data["eventType"] == "removed" then
     log.d("USB disconnect: productName '" .. data["productName"] .. "'; vendorID '" .. data["vendorID"] .. "'; productID '" .. data["productID"] .. "'")
 
     if isMainMouse then
@@ -60,8 +24,12 @@ function usbCallback(data)
       -- on disconnect from 'USB Optical Mouse',
       -- if this machine is my personal desktop Mac and it's using the onboard speakers,
       -- mute it:
-      if isHomeDeskMacStudio and hs.audiodevice.current()["name"] == "Mac Studio Speakers" then
-        hs.audiodevice.defaultOutputDevice():setMuted(true)
+      if isHomeDeskMacStudio then
+        local isMacStudioSpeakers = hs.audiodevice.current()["name"] == "Mac Studio Speakers"
+        local isMonitorSpeakers = hs.audiodevice.current()["name"] == "Studio Display Speakers"
+        if isMacStudioSpeakers or isMonitorSpeakers then
+          hs.audiodevice.defaultOutputDevice():setMuted(true)
+        end
       end
     end
 
@@ -80,14 +48,6 @@ function usbCallback(data)
           eyeContactControlApp:kill()
         end
       end
-      -- handMirrorApp = hs.application.get("net.rafaelconde.Hand-Mirror")
-      -- if handMirrorApp then
-      --   handMirrorApp:kill()
-      -- end
-      -- elgatoCcApp = hs.application.get("com.corsair.ControlCenter")
-      -- if elgatoCcApp then
-      --   elgatoCcApp:kill()
-      -- end
       if isHomeDeskMacStudio then
         zoomApp = hs.application.get("us.zoom.xos")
         if zoomApp then
