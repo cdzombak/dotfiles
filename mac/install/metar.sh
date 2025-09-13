9e9a2b8fb300625c2587a0853f009ea0da9fc527
Chris Dzombak <chris@dzombak.com>
2025-09-13 19:26:01 -0400

    fix metar install

--- mac/install/metar.sh
+++ mac/install/metar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone https://github.com/cdzombak/metar.git /tmp/metar
+git clone https://github.com/RyuKojiro/metar.git /tmp/metar
 pushd /tmp/metar
 make install
 popd
