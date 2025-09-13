30327eaecb350543e61e25d4967aa790a637f5d8
Chris Dzombak <chris@dzombak.com>
2025-09-13 19:28:14 -0400

    fix metar install

--- mac/install/metar.sh
+++ mac/install/metar.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+trap 'echo "exit $? at line $LINENO from: $BASH_COMMAND"' ERR
 
 git clone https://github.com/RyuKojiro/metar.git /tmp/metar
 pushd /tmp/metar
 sudo make install
 popd
-rm -rf /tmp/metar
+sudo rm -rf /tmp/metar
