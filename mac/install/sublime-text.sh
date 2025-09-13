e8b15660b3fed48e71e728d258c83124e590213c
Chris Dzombak <chris@dzombak.com>
2025-09-13 19:29:23 -0400

    fix sublime text install

--- mac/install/sublime-text.sh
+++ mac/install/sublime-text.sh
@@ -12,7 +12,7 @@ popd
 SUBLIMETEXT_PKGS_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages"
 mkdir -p "$SUBLIMETEXT_PKGS_DIR"
 pushd "$SUBLIMETEXT_PKGS_DIR"
-echo "This might be a good point to generate a GitHub personal access token for this device, to be stored in the local login keychain:" $white
-echo "https://github.com/settings/tokens" $cyan
+echo "This might be a good point to generate a GitHub personal access token for this device, to be stored in the local login keychain:"
+echo "https://github.com/settings/tokens"
 git clone git@github.com:cdzombak/sublime-text-config.git User
 popd
