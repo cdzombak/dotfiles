008266e3cdc400a766dd754c5f108744a6425303
Chris Dzombak <chris@dzombak.com>
2025-09-22 16:53:39 -0400

    fix: use dynamic latest.php URL for RemoteForMac download

--- mac/install/remote-for-mac.sh
+++ mac/install/remote-for-mac.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'remotehelperapp-work')
 pushd "$TMP_DIR"
-wget --quiet -O "RemoteForMac.zip" "https://cherpake.com/download-latest"
+wget -O "RemoteForMac.zip" "https://cherpake.com/latest.php?os=mac"
 unzip ./RemoteForMac.zip
 sudo installer -pkg ./Remote-for-Mac-*.pkg -target /
 popd
