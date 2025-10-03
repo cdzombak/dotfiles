fa115a5154934109331bd98ffdd94faabb4386cc
Chris Dzombak <chris@dzombak.com>
2025-10-03 12:23:18 -0400

    remove some old k8s aliases

--- mac/zsh/.zsh/k8s.zsh
+++ mac/zsh/.zsh/k8s.zsh
@@ -1,7 +1,9 @@
-# Kubernetes:
+
 alias kubectl="k8s-ctx-show; kubectl"
-. ~/.kubectl_aliases
-alias kprod="kubectl --namespace production"
-alias kst="kubectl --namespace staging"
 alias kl='kubectl logs -f'
 alias kgpa='kubectl get pods --all-namespaces -owide'
+
+. ~/.kubectl_aliases
+
+# alias kprod="kubectl --namespace production"
+# alias kst="kubectl --namespace staging"
